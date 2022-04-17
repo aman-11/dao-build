3dea1003673db1f35f2ff5e8bf74623f6e3d8165
Aayush <aayushaman11@gmail..com>
2022-04-18 01:11:29 +0530

    added the authentication for user

--- dao-ui/pages/_app.js
+++ dao-ui/pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl="https://xdcuqwogn1hj.usemoralis.com:2053/server"
+      appId="zBxmxU32mZXBku23hdyhkA7pzuAMBWwVb20CqBbR"
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- dao-ui/pages/index.js
+++ dao-ui/pages/index.js
@@ -3,8 +3,46 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import coverImage from "../public/cover.svg";
 import Metamask from "../components/Metamask";
+import { useMoralis } from "react-moralis";
+import ErrorMessage from "../components/ErrorMessage";
 
 export default function Home() {
+  const {
+    authenticate,
+    authError,
+    isAuthenticated,
+    user,
+    logout,
+    Moralis,
+    enableWeb3,
+  } = useMoralis();
+  const [account, setAccount] = useState("");
+
+  // console.log("user", user);
+  // console.log("account", account);
+  // user.atttributes has all the necessary details required for retrv of wallet info
+  // console.log("user attr", user?.attributes.ethAddress);
+  // console.log("isAuthenticated", isAuthenticated);
+
+  const onPageLoadAction = async () => {
+    // console.log("user", user);
+    if (isAuthenticated) setAccount(user?.attributes.ethAddress);
+  };
+
+  useEffect(() => {
+    onPageLoadAction();
+    enableWeb3();
+    // Moralis.onWeb3Enabled((result) => {
+    //   console.log(result);
+    // });
+
+    Moralis.onAccountChanged(function (account) {
+      logout();
+      authenticate();
+      onPageLoadAction();
+    });
+  }, [isAuthenticated]);
+
   return (
     <div>
       <Head>
@@ -12,18 +50,23 @@ export default function Home() {
         <meta property="og:title" content="Whitelist" key="title" />
       </Head>
 
-      <header className="flex border-b justify-end bg-gray-500">
-        <div className="flex text-xs flex-col mb-2 mt-2 space-y-1 text-white">
-          <p className=" mr-16 text-xs font-normal">
-            Hello, 0x024nj23beds233e3dsda
-          </p>
-          <div>
-            <button className="text-sm font-bold underline underline-offset-2">
-              Logout
-            </button>
+      {isAuthenticated && (
+        <header className="flex border-b justify-end bg-gray-900">
+          <div className="flex text-xs flex-col mt-2 mb-2 space-y-2 text-white">
+            <p className=" mr-16 text-xs font-normal">
+              Hello, {account.slice(0, 4)}...{account.slice(account.length - 4)}
+            </p>
+            <div>
+              <button
+                onClick={logout}
+                className="text-sm font-bold underline underline-offset-2"
+              >
+                Logout
+              </button>
+            </div>
           </div>
-        </div>
-      </header>
+        </header>
+      )}
 
       <main className="min-h-[85vh] pl-24 pr-24 flex items-center justify-center">
         <div className="flex-1 space-y-3">
@@ -45,27 +88,40 @@ export default function Home() {
             Total Number of Proposals:{" "}
             <span className="underline font-semibold">1 ETH</span>
           </p>
+          {isAuthenticated ? (
+            <div>
+              <button
+                type="button"
+                className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+              >
+                Create Proposal
+              </button>
+              <button
+                type="button"
+                className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+              >
+                View Proposals
+              </button>
+              <br />
+              <p
+                onClick={authenticate}
+                className="font-base mt-1 underline underline-offset-1 text-sm cursor-pointer hover:text-gray-700"
+              >
+                Connect with different account?
+              </p>
+            </div>
+          ) : (
+            <button
+              onClick={authenticate}
+              type="button"
+              className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700"
+            >
+              <Metamask />
+              Connect with MetaMask
+            </button>
+          )}
 
-          <button
-            type="button"
-            class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-          >
-            Create Proposal
-          </button>
-          <button
-            type="button"
-            class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-          >
-            View Proposals
-          </button>
-
-          <button
-            type="button"
-            class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700"
-          >
-            <Metamask />
-            Connect with MetaMask
-          </button>
+          {authError && <ErrorMessage message={authError.message} />}
         </div>
 
         <Image src={coverImage} objectFit="cover" alt="Dev" />
